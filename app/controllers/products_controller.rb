3827d7f1ca68c49ab47c1407682cbd00e241c33d
akane0915 <asialkane@gmail.com>
2017-07-07 10:37:15 -0700

    add ability to edit and delete product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,24 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Product.find(params[:id]).destroy
+    redirect_to products_path
+  end
+
 private
   def product_params
     params.require(:product).permit(:name, :description, :price, :image, :origin, :variety)
