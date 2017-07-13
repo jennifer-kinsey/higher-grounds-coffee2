c091640485303f3f542e835e6ef4ea5780f19b7a
Asia Kane and Jennifer Kinsey <asia@>
2017-07-13 09:06:32 -0700

    adds ajax for admins to edit product without leaving show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,7 +38,10 @@ class ProductsController < ApplicationController
     @product= Product.find(params[:id])
     if @product.update(product_params)
       flash[:notice] = "Product updated."
-      redirect_to products_path
+      respond_to do |format|
+        format.html { redirect_to product_path(@product) }
+        format.js
+      end
     else
       render :edit
     end
